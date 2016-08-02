088f3e9600517456860266a1c9e54ef0f362f8eb
Rafael Fiume <rafael.fiume@yahoo.it>
2016-08-02 14:47:33 +0100

    Spec test skeleton using Clairvoyance

---
+++ eCore/src/test/scala/com/equalexperts/ehelp/ecore/TrackCalamitySpec.scala
@@ -0,0 +1,16 @@
+package com.equalexperts.ehelp.ecore
+
+import clairvoyance.specs2.{ClairvoyantContext, ClairvoyantSpec}
+
+class TrackCalamitySpec extends ClairvoyantSpec {
+
+  "The eCore" should {
+    "track a calamity when someone asks for help using our services" in new context {
+
+    }.pendingUntilFixed("working in progress")
+  }
+}
+
+trait context extends ClairvoyantContext {
+
+}
