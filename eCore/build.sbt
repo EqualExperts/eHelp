cc5baea4179bc2027974cf9f99579e5c907e38a2
Rafael Fiume <rfiume@equalexperts.com>
2016-08-11 18:35:35 +0100

    1: Using httpGet to simple requests (WiP)
        Part of writing <<Track Calamity>> spec test
    
    @rafaelfiume

--- eCore/build.sbt
+++ eCore/build.sbt
@@ -8,7 +8,7 @@ scalaVersion := "2.11.8"
 scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
 
 libraryDependencies ++= {
- val akkaV       = "2.4.3"
+ val akkaV       = "2.4.8"
   Seq(
     "com.typesafe.akka"       %% "akka-actor"                        % akkaV,
     "com.typesafe.akka"       %% "akka-stream"                       % akkaV,
--- eCore/src/test/scala/com/equalexperts/ehelp/ecore/TrackCalamitySpec.scala
+++ eCore/src/test/scala/com/equalexperts/ehelp/ecore/TrackCalamitySpec.scala
@@ -2,7 +2,14 @@ package com.equalexperts.ehelp.ecore
 
 import java.util.{Calendar, Date}
 
-import clairvoyance.specs2.{ClairvoyantContext, ClairvoyantSpec};
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
+import akka.stream.ActorMaterializer
+import clairvoyance.specs2.{ClairvoyantContext, ClairvoyantSpec}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
 class TrackCalamitySpec extends ClairvoyantSpec {
 
@@ -15,11 +22,15 @@ class TrackCalamitySpec extends ClairvoyantSpec {
 
       thenICanSeeThatSomebodyAskedForHelp(toEscapeFrom("flood"), at("Santana neighborhood in São Paulo"), requesting("escaping"));
 
-    }//.pendingUntilFixed("working in progress")
+    }.pendingUntilFixed("working in progress")
   }
 }
 
 trait context extends ClairvoyantContext {
+
+  implicit val system = ActorSystem("eCore")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
 
   val server = new Server();
 
@@ -31,7 +42,19 @@ trait context extends ClairvoyantContext {
   }
 
   def thenICanSeeThatSomebodyAskedForHelp(value: Any, value1: Any, value2: Any) = {
+    httpGet("http://localhost:9000/hello")
+  }
 
+  def httpGet(path: String) : Unit = {
+    val futureResponse: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = path))
+    val response: HttpResponse = Await.result(futureResponse, 1 second)
+
+    response match {
+      case HttpResponse(StatusCodes.OK, headers, entity, _) =>
+        println("Got response, body: " + entity.toStrict(1 seconds).map(_.data.decodeString("UTF-8")))
+      case HttpResponse(code, _, _, _) =>
+        println("Request failed, response code: {}", code)
+    };
   }
 
   def dueTo(calamity: String): Calamity = new Calamity(calamity)
